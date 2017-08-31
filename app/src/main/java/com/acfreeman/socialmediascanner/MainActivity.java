3a537636fd382948bc1bb90727a52f768a8c626d
acfreeman2 <andrew.freeman@cawb.com>
2017-08-31 18:52:31 -0500

    Change bottom navigation icons

--- app/src/main/java/com/acfreeman/socialmediascanner/MainActivity.java
+++ app/src/main/java/com/acfreeman/socialmediascanner/MainActivity.java
@@ -17,14 +17,14 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                case R.id.navigation_show:
+                    mTextMessage.setText(R.string.title_qr);
                     return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                case R.id.navigation_friends:
+                    mTextMessage.setText(R.string.title_friends);
                     return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                case R.id.navigation_camera:
+                    mTextMessage.setText(R.string.title_camera);
                     return true;
             }
             return false;
