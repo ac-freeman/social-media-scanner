af5447cdaa749761548d3ec5067c5668e3eab0bd
acfreeman2 <afreema4@samford.edu>
2017-09-13 10:15:39 -0500

    Optimize QR code generation
    Add early scan implementation

--- app/src/main/java/com/acfreeman/socialmediascanner/MainActivity.java
+++ app/src/main/java/com/acfreeman/socialmediascanner/MainActivity.java
@@ -7,11 +7,14 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import com.google.zxing.*;
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.EncodeHintType;
@@ -25,8 +28,12 @@ import com.google.zxing.client.result.ParsedResult;
 import com.google.zxing.client.result.ResultParser;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.common.HybridBinarizer;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 import com.google.zxing.qrcode.QRCodeReader;
 import com.google.zxing.qrcode.QRCodeWriter;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
+import com.journeyapps.barcodescanner.CaptureActivity;
 
 import static android.R.attr.height;
 import static android.R.attr.left;
@@ -58,7 +65,7 @@ public class MainActivity extends AppCompatActivity {
                     return true;
                 case R.id.navigation_camera:
                     frameLayout.removeAllViews();
-//                    scanCode();
+                    scanCode();
 
                     return true;
             }
@@ -75,13 +82,17 @@ public class MainActivity extends AppCompatActivity {
 //            int height = mImageView.getHeight();
             int width = frameLayout.getWidth();
             int height = frameLayout.getHeight();
-            BitMatrix bitMatrix = writer.encode("test", BarcodeFormat.QR_CODE, width, height);
-            Bitmap bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
-            for (int i = 0; i < width; i++) {
-                for (int j = 0; j < height; j++) {
-                    bitmap.setPixel(i, j, bitMatrix.get(i, j) ? Color.BLACK: Color.WHITE);
-                }
-            }
+//            BitMatrix bitMatrix = writer.encode("test", BarcodeFormat.QR_CODE, width, height);
+//            Bitmap bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+//            for (int i = 0; i < width; i++) {
+//                for (int j = 0; j < height; j++) {
+//                    bitmap.setPixel(i, j, bitMatrix.get(i, j) ? Color.BLACK: Color.WHITE);
+//                }
+//            }
+            MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
+            BitMatrix bitMatrix = multiFormatWriter.encode("test", BarcodeFormat.QR_CODE, width, height);
+            BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
+            Bitmap bitmap = barcodeEncoder.createBitmap(bitMatrix);
             mImageView.setImageBitmap(bitmap);
         } catch (WriterException e) {
             e.printStackTrace();
@@ -98,23 +109,37 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-//    private void scanCode(){
-//        MultiFormatReader reader = new MultiFormatReader();
-//        LuminanceSource source = new PlanarYUVLuminanceSource(yuvData, dataWidth, dataHeight, left, top, width, height, false);
-//        BinaryBitmap bitmap = new BinaryBitmap(new HybridBinarizer(source));
-//        Result result;
-//        try {
-//            result = reader.decode(bitmap);
-//            if (result != null) {
-//                mDialog.setTitle("Result");
-//                mDialog.setMessage(result.getText());
-//                mDialog.show();
+    private void scanCode(){
+//        Intent intent = new Intent(getApplicationContext(),CaptureActivity.class);
+//        intent.setAction("com.google.zxing.client.android.SCAN");
+//        intent.putExtra("SAVE_HISTORY", false);
+//        startActivityForResult(intent, 0);
+
+        new IntentIntegrator(this).initiateScan();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        if (requestCode == 0) {
+//            if (resultCode == RESULT_OK) {
+//                String contents = data.getStringExtra("SCAN_RESULT");
+//                Log.d("TAG", "contents: " + contents);
+//            } else if (resultCode == RESULT_CANCELED) {
+//// Handle cancel
+//                Log.d("TAG", "RESULT_CANCELED");
 //            }
-//        } catch (NotFoundException e) {
-//            // TODO Auto-generated catch block
-//            e.printStackTrace();
 //        }
-//    }
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if(result != null) {
+            if(result.getContents() == null) {
+                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this, "Scanned: " + result.getContents(), Toast.LENGTH_LONG).show();
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
