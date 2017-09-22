d792784ebaf7b97fff6652cd77c2f1a317b42766
rayherbert2014 <rayherbert2014@gmail.com>
2017-09-22 11:06:08 -0500

    Integrate linkedin SDK

--- app/src/main/java/com/acfreeman/socialmediascanner/RegistrationInformation.java
+++ app/src/main/java/com/acfreeman/socialmediascanner/RegistrationInformation.java
@@ -2,33 +2,22 @@ package com.acfreeman.socialmediascanner;
 
 import android.content.ContentValues;
 import android.content.Intent;
-import android.graphics.Color;
+import android.content.SharedPreferences;
+import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Point;
 import android.os.Build;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Display;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
-import android.widget.TextView;
-import android.graphics.Color;
-import android.os.Bundle;
-import android.widget.Button;
-import android.widget.RelativeLayout;
 import android.widget.RelativeLayout.LayoutParams;
-import android.content.SharedPreferences;
-import android.database.sqlite.SQLiteDatabase;
-import android.os.Bundle;
-import android.preference.PreferenceManager;
-import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 
 
 public class RegistrationInformation extends AppCompatActivity {
