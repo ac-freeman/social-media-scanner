d32f2a3196c1e34630e7446e5d4ed55b65ee78c7
acfreeman2 <afreema4@samford.edu>
2017-10-02 18:28:40 -0500

    Fix permissions feature

--- app/src/main/java/com/acfreeman/socialmediascanner/MainActivity.java
+++ app/src/main/java/com/acfreeman/socialmediascanner/MainActivity.java
@@ -296,32 +296,25 @@ public class MainActivity extends AppCompatActivity implements ZXingScannerView.
      * Uses https://github.com/dm77/barcodescanner
      * Requires camera permission in settings
      */
+     private final int MY_PERMISSIONS_REQUEST_CAMERA = 1;
     private void scanCode(){
 
         if (ContextCompat.checkSelfPermission(MainActivity.this,
-                Manifest.permission.READ_CONTACTS)
+                Manifest.permission.CAMERA)
                 != PackageManager.PERMISSION_GRANTED) {
 
-            // Should we show an explanation?
-            if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,
-                    Manifest.permission.READ_CONTACTS)) {
 
-                // Show an explanation to the user *asynchronously* -- don't block
-                // this thread waiting for the user's response! After the user
-                // sees the explanation, try again to request the permission.
-
-            } else {
 
                 // No explanation needed, we can request the permission.
 
                 ActivityCompat.requestPermissions(MainActivity.this,
-                        new String[]{Manifest.permission.READ_CONTACTS},
-                        MY_PERMISSIONS_REQUEST);
+                        new String[]{Manifest.permission.CAMERA},
+                        MY_PERMISSIONS_REQUEST_CAMERA);
 
                 // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
                 // app-defined int constant. The callback method gets the
                 // result of the request.
-            }
+
         }
 
 
@@ -540,6 +533,31 @@ public class MainActivity extends AppCompatActivity implements ZXingScannerView.
         String name = mArgs.getString("name");
         showNoticeDialog(name);
     }
+
+    @Override
+public void onRequestPermissionsResult(int requestCode,
+        String permissions[], int[] grantResults) {
+    switch (requestCode) {
+        case MY_PERMISSIONS_REQUEST_CAMERA: {
+            // If request is cancelled, the result arrays are empty.
+            if (grantResults.length > 0
+                && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                // permission was granted, yay! Do the
+                // contacts-related task you need to do.
+
+            } else {
+
+                // permission denied, boo! Disable the
+                // functionality that depends on this permission.
+            }
+            return;
+        }
+
+        // other 'case' lines to check for other
+        // permissions this app might request
+    }
+}
 
 
     /**
