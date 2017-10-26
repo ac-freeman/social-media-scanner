9a01cae8628dec2697cab42c2173a85ff68e7a5d
rayherbert2014 <rayherbert2014@gmail.com>
2017-10-25 23:29:53 -0500

    Add Google id to local database

--- app/src/main/java/com/acfreeman/socialmediascanner/GoogleLoginActivity.java
+++ app/src/main/java/com/acfreeman/socialmediascanner/GoogleLoginActivity.java
@@ -8,21 +8,36 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import com.acfreeman.socialmediascanner.db.LocalDatabase;
+import com.acfreeman.socialmediascanner.db.Owner;
+import com.acfreeman.socialmediascanner.db.Social;
 import com.acfreeman.socialmediascanner.social.SocialMediaLoginActivity;
 import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.GoogleApiClient;
+
+import java.util.List;
 
 public class GoogleLoginActivity extends FragmentActivity {
 
     private int RC_SIGN_IN = 9001;
 
+    public LocalDatabase database;
+    public List<Owner> owners;
+    public Owner owner;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_google_login);
+
+        database = new LocalDatabase(getApplicationContext());
+        owners = database.getAllOwner();
+        owner = owners.get(0);
 
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestEmail()
@@ -60,5 +75,29 @@ public class GoogleLoginActivity extends FragmentActivity {
             }
         });
 
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            if (result.isSuccess()) {
+                GoogleSignInAccount acct = result.getSignInAccount();
+                String googleId = acct.getId();
+                //String id2 = acct.getIdToken();
+
+                /////add to database//////////
+                Social google = new Social(owner.getId(),"go",googleId);
+                database.addSocial(google);
+                //////////////////////////////
+
+                //Toast.makeText(GoogleLoginActivity.this, "Google_id: " + database.getSocialCount(), Toast.LENGTH_LONG).show();
+            } else {
+                Log.e("CCCCCCCCCCCCCCC", "Could not login");
+            }
+        }
     }
 }
