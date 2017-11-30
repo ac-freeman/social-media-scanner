d8f4b69c3a658d8a2f2d349a57a4bd3b60776bcd
edgarhflores <eflores@samford.edu>
2017-11-30 11:04:41 -0600

    added views to Qr Modes UI

--- app/src/main/java/com/acfreeman/socialmediascanner/QrModesActivity.java
+++ app/src/main/java/com/acfreeman/socialmediascanner/QrModesActivity.java
@@ -2,6 +2,9 @@ package com.acfreeman.socialmediascanner;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.Toast;
 
 public class QrModesActivity extends AppCompatActivity {
 
@@ -9,7 +12,18 @@ public class QrModesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qr_modes);
+        Switch allSwitch = (Switch) findViewById(R.id.switchAll);
+        allSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (b){
 
+                    Toast.makeText(QrModesActivity.this, "All QR toggles turned on", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(QrModesActivity.this, "Toggle turned off", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
     //Step 1: Add option to toggle all information
